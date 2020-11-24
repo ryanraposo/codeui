f05a6af954d953904be93a05d8173bd8f28bfe51
Ryan Raposo <raposo.ryan@gmail.com>
2020-11-24 14:15:06 -0500

    🔮 Update project settings
    Change ESLint config
    Add formatting (prettier) rules
    Fix vscode workspace settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,48 +12,20 @@ https://github.com/typescript-eslint/tslint-to-eslint-config/blob/master/docs/FA
 Happy linting! 💖
 */
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
+    parser: '@typescript-eslint/parser',
+    env: {
+        es6: true,
+        node: true
     },
-    "extends": [
-        "prettier",
-        "prettier/@typescript-eslint"
+    extends: [
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module"
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module'
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "@typescript-eslint/member-delimiter-style": [
-            "warn",
-            {
-                "multiline": {
-                    "delimiter": "semi",
-                    "requireLast": true
-                },
-                "singleline": {
-                    "delimiter": "semi",
-                    "requireLast": false
-                }
-            }
-        ],
-        "@typescript-eslint/naming-convention": "warn",
-        "@typescript-eslint/no-unused-expressions": "warn",
-        "@typescript-eslint/semi": [
-            "warn",
-            "always"
-        ],
-        "curly": "warn",
-        "eqeqeq": [
-            "warn",
-            "always"
-        ],
-        "no-redeclare": "warn",
-        "no-throw-literal": "warn"
-    }
+    plugins: ['@typescript-eslint'],
+    rules: {}
 };
